2d0be20088a1d1c26dd31a4aa0ae50e6eeb69ce2
marianarmorgado <marianarmorgado@gmail.com>
2021-12-22 15:17:28 +0200

    improve bmi histograms

--- scripts/exercises.R
+++ scripts/exercises.R
@@ -9,15 +9,19 @@ students$bmi <- students$weight / sqrt(students$height / 100)
 # Does the body mass index follow the normal distribution?
 
 attach(students)
-windows()
+windows(8, 10)
 par(mfrow = c(2, 1))
     hist(bmi[gender == "1"],
-        xlab = "BMI",
         main = "BMI of women",
+        xlab = "BMI",
+        xlim = c(30, 90),
+        ylim = c(0, 200),
         col = "#DDAAC4"
         )
      hist(bmi[gender == "2"],
-        xlab = "BMI",
         main = "BMI of men",
+        xlab = "BMI",
+        xlim = c(30, 90),
+        ylim = c(0, 40),
         col = "#AAC7DD"
         )

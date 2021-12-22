4d632064c662d6a237a3742fb73e744434c3b46b
marianarmorgado <marianarmorgado@gmail.com>
2021-12-22 19:02:09 +0200

    exercise 4

--- scripts/exercises.R
+++ scripts/exercises.R
@@ -1,6 +1,7 @@
 load(url("http://ph.emu.ee/~ktanel/DK_0016/students.RData"))
 
-#-----1-----
+#--------------------------------------------------------------
+#-----1--------------------------------------------------------
 # Calculate the students’ body mass index (as a new variable)...
 
 students$bmi <- students$weight / ((students$height / 100)^2)
@@ -26,7 +27,8 @@ par(mfrow = c(1, 2))
         col = "#AAC7DD"
         )
 
-#-----2-----
+#--------------------------------------------------------------
+#-----2--------------------------------------------------------
 # What is the average (± standard deviation) body mass index of men and women?
 
 mean(students$bmi [students$gender == 1], na.rm = TRUE)
@@ -40,11 +42,12 @@ sd(students$bmi[students$gender == 2], na.rm = TRUE)
 
 t.test(bmi ~ gender, data = students)
 
-#-----3-----
-#Create a new variable ’sport01’ with value zero,
+#--------------------------------------------------------------
+#-----3--------------------------------------------------------
+# Create a new variable ’sport01’ with value zero,
 # if student does not practice sport (sport=1), and value one,
 # if student practices sport (sport>1).
-#You can use the following command:
+# ou can use the following command:
 
 students$sport01 <- factor(
     students$sport,
@@ -53,8 +56,8 @@ students$sport01 <- factor(
 )
 
 # How big is the percentage of students practicing sport
-#(with 95% confidence interval)? Is the percentage of
-#students practicing sport statistically significant from 75%?
+# (with 95% confidence interval)? Is the percentage of
+# students practicing sport statistically significant from 75%?
 
 table(students$sport01)
 prop.test(
@@ -62,3 +65,17 @@ prop.test(
     c(124 + 535, 124 + 535)
 )
 prop.test(535, 660, p = 0.75)
+
+#--------------------------------------------------------------
+#-----4--------------------------------------------------------
+# What about the percentage of sporting students among men and women?
+# Are these proportions statistically significantly different?
+
+table(
+    students$gender,
+    students$sport01 == "1"
+)
+prop.test(
+    c(408, 127),
+    c(408 + 103, 127 + 21)
+)
